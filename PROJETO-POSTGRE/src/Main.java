72e33eff4ed5b506fb256715840d337040d4b977
Marcos Cavalcanti <72262936+Marcos-Cavalcanti21@users.noreply.github.com>
2022-06-01 20:09:21 -0300

    Alterações nas classes

--- PROJETO-POSTGRE/src/Main.java
+++ PROJETO-POSTGRE/src/Main.java
@@ -1,8 +1,115 @@
 import VIEW.Menu;
 
+import java.sql.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
+   // public static void main(String[] args) {
+       // Menu.inicio();
+
+//        Scanner get = new Scanner(System.in);
+//
+//        int[] valor = { 0, 0, 0, 0};
+//
+//        System.out.println("N1º");
+//        valor[0] = get.nextInt();
+//
+//        System.out.println("N2º");
+//        valor[1] = get.nextInt();
+//
+//        System.out.println("N3º");
+//        valor[2] = get.nextInt();
+//
+//        System.out.println("N4º");
+//        valor[3] = get.nextInt();
+//
+//        System.out.println(Arrays.stream(valor).min());
+
+    //}
+
+    public static void menu() {
+        while(0==0) {
+            System.out.println("Escolha a opção desejada");
+            System.out.println("1 - Cadastrar Aluno");
+            System.out.println("2 - Cadastrar Professor");
+            System.out.println("3 - Exibir dados do Aluno");
+            System.out.println("4 - Exibir dados do Professor");
+            System.out.println("S - SAIR");
+            op = read.nextLine();
+            switch (op) {
+                case "1": {
+                    a = new Aluno();
+                    System.out.println("Digite o nome do aluno:");
+                    String nome = read.nextLine();
+                    a.setNome(nome);
+
+                    System.out.println("Digite a idade do aluno:");
+                    String idade = read.nextLine();
+                    a.setIdade(idade);
+
+                    System.out.println("Digite o CPF do aluno:");
+                    String cpf = read.nextLine();
+                    a.setCpf(cpf);
+
+                    System.out.println("Digite a matrícula do aluno:");
+                    String matricula = read.nextLine();
+                    a.setMatricula(matricula);
+
+                    break;
+
+                }
+                case "2": {
+                    p = new Professor();
+                    System.out.println("Digite o nome do professor:");
+                    String nome = read.nextLine();
+                    p.setNome(nome);
+
+                    System.out.println("Digite a idade do professor:");
+                    String idade = read.nextLine();
+                    p.setIdade(idade);
+
+                    System.out.println("Digite o CPF do professor:");
+                    String cpf = read.nextLine();
+                    p.setCpf(cpf);
+
+                    System.out.println("Digite  a turma do professor:");
+                    String turma = read.nextLine();
+                    p.setTurma(turma);
+
+                    break;
+
+                }
+                case "3": {
+                    System.out.println("DADOS DO ALUNO");
+                    a.exibirDadosAluno();
+
+                    break;
+                }
+                case "4": {
+                    System.out.println("DADOS DO PROFESSOR");
+                    p.exibirDadosProfessor();
+
+                    break;
+                }
+                default:
+                    System.out.println("Informação incorreta");
+            }
+
+        }
+
+    }
+
     public static void main(String[] args) {
-        Menu.inicio();
+
+        Aluno a = null;
+        Professor p = null;
+
+        do {
+            menu();
+
+
+        } while (op != "S");
     }
 }
 
